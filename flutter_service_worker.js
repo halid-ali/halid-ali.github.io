a8d6a29fcc4b1d56d8c13e71a24350a827e7091c
Halid Ali <halid.ali@methodpark.de>
2021-03-07 17:36:24 +0100

    login and register features

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,9 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  ".git/COMMIT_EDITMSG": "55a98d2f133bb742c384632fefda4f24",
+  ".git/COMMIT_EDITMSG": "739d9e2794008bc3523422ad0ff012ac",
 ".git/config": "318feaa4f35d1c05308abd43a3e10829",
 ".git/description": "a0a7c3fff21f2aea3cfa1d0316dd816c",
+".git/FETCH_HEAD": "709d117ec46e288fcc8a27b35374af7c",
 ".git/HEAD": "4cf2d64e44205fe628ddd534e1151b58",
 ".git/hooks/applypatch-msg.sample": "ce562e08d8098926a3862fc6e7905199",
 ".git/hooks/commit-msg.sample": "579a3c1e12a1e74a98169175fb913012",
@@ -19,19 +20,24 @@ const RESOURCES = {
 ".git/hooks/pre-receive.sample": "2ad18ec82c20af7b5926ed9cea6aeedd",
 ".git/hooks/prepare-commit-msg.sample": "2b5c047bdb474555e1787db32b2d2fc5",
 ".git/hooks/update.sample": "7bf1fcc5f411e5ad68c59b68661660ed",
-".git/index": "0489c274338b03c3ed87ba60a60baa1a",
+".git/index": "1fded2783e9c43df4da70a88bc2793f4",
 ".git/info/exclude": "036208b4a1ab4a235d75c181e685e5a3",
-".git/logs/HEAD": "43b142fab1f9642c60a37e432e78d414",
-".git/logs/refs/heads/master": "43b142fab1f9642c60a37e432e78d414",
-".git/logs/refs/remotes/origin/master": "218ca4982e2f81c71a06d4870b3b9960",
+".git/logs/HEAD": "cde71fdaa274bc826fa3457a6fd2b31b",
+".git/logs/refs/heads/master": "cde71fdaa274bc826fa3457a6fd2b31b",
+".git/logs/refs/remotes/origin/master": "757a47f6216f437941dcaecdf8274089",
 ".git/objects/16/57523744657e3d5b4d91e1d934c8a315afe471": "3a5b73f2057ba3e9d6fffe53ba29937b",
 ".git/objects/1d/b67ff370924a62326bf208f353e925c21f2204": "ec018403ab2fc1c3edc8c82f3f1d9d2f",
 ".git/objects/20/5bb5db271c6d8de8399864c7bb9b917f638893": "c993b22f115d7f3ae6d5b7b212806539",
 ".git/objects/27/bed7ee567a99ce7739c6bd16a559c8957ed055": "9473d8884f373fc1738452f894f2beef",
+".git/objects/2f/cd2efcb0e2418d421d8f0f5d6084db97ac57a0": "0890a67586a17885129239de450d1032",
+".git/objects/32/17b54985d8bdf8f4711d0f6240d6ff04c3171c": "cb648340ca30292c61a3ae942090cedb",
 ".git/objects/3a/bf18c41c58c933308c244a875bf383856e103e": "30790d31a35e3622fd7b3849c9bf1894",
 ".git/objects/50/8377c2a18a56b45ec1eb0492e73e05e2d9e398": "e6766709633510bbd0aa67844a3b03b6",
 ".git/objects/58/ad9e8752a5663a44f0ddc4d6c8389850bc6b80": "c82f43adfe7675a6003136bf437994c4",
+".git/objects/68/f3b42339c587a421e43465f8623fee4bbe470c": "c0982c8c984683cdf6c451145937d741",
+".git/objects/6c/c5e22e6c4062dbdf77a6db414923e3ddd7ac3f": "1c71a8f969022bb3342c85fd2357a0f1",
 ".git/objects/6d/de9367edf93bc73a9bed4ff0701038dbd1a4b1": "70e92a56a1c4230db96a302ebad1cacd",
+".git/objects/6e/1afc9ab1bd431fdbc4afe6809fd2280cf5adb3": "2f35d69103be617fd98db74e8d331eb4",
 ".git/objects/88/cfd48dff1169879ba46840804b412fe02fefd6": "e42aaae6a4cbfbc9f6326f1fa9e3380c",
 ".git/objects/89/8dd1c766b828075a777f777ceb0d9b880b5de3": "03f58d8d0d83007080f9b29a9070b229",
 ".git/objects/8a/aa46ac1ae21512746f852a42ba87e4165dfdd1": "1d8820d345e38b30de033aa4b5a23e7b",
@@ -39,23 +45,34 @@ const RESOURCES = {
 ".git/objects/a8/3a17d54b6dc2156c855c6580301cefc9c8d017": "6525101be8d70b808a809af9c83fa4ad",
 ".git/objects/b7/49bfef07473333cf1dd31e9eed89862a5d52aa": "36b4020dca303986cad10924774fb5dc",
 ".git/objects/bc/1db58fb49ef942b5f7ae21a80f8420d58c2a0d": "b5e771096a4f2e47521dd8b5bb1ba107",
+".git/objects/bd/954c95fff4c557f9f7108b9e8b70cab40f8aa6": "63e85ae491f76738fb0bc8e7c1f71c03",
+".git/objects/bf/4d7278c889c552827315fd0845d75f8efc4c20": "9e261c81b0f9bd48c837d28c10f0fbe2",
+".git/objects/c4/192631f25b34d77a7f159aa0da0e3ae99c4ef4": "546c588f67767790fc70913da1a77878",
 ".git/objects/f2/5787c93ad00665f0f797a43ab397f0628a273e": "59e425d1456034072d902c75d18ec75f",
 ".git/objects/f9/a1f6f5bc3585054c3fad963bfc09b2cbf11daf": "fa4bb1a3829497fe8dfac801b08aecfb",
 ".git/objects/fb/814f3cbdc87b4825471beb47b248e3905458cd": "ed1a38777fdd4553538df9abf9d9a59d",
-".git/refs/heads/master": "32480de5c2ddff99f97160e1d5fc076d",
-".git/refs/remotes/origin/master": "32480de5c2ddff99f97160e1d5fc076d",
-"assets/AssetManifest.json": "99914b932bd37a50b983c5e7c90ae93b",
+".git/ORIG_HEAD": "32480de5c2ddff99f97160e1d5fc076d",
+".git/refs/heads/master": "bb049bd7b1279a1edaef7574d28b0747",
+".git/refs/remotes/origin/master": "bb049bd7b1279a1edaef7574d28b0747",
+"assets/AssetManifest.json": "c5b405df7a99bd2b664b2d39ea673d2f",
+"assets/assets/chronometer.png": "ab8378ba1dc220e174f1fb0eac563fde",
+"assets/assets/deutschland.svg": "9f6bb7dfdfd1ff896c0261f7261428ca",
+"assets/assets/england.svg": "8987950dedd49c12db107313dc0f3b85",
+"assets/assets/turkiye.svg": "2d51b2a873c437aa4644b99f9a9a165a",
 "assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
 "assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
-"assets/NOTICES": "4cfba56f84521e25054c1be7c1c8fac8",
+"assets/NOTICES": "29948a699a54bd0f5d80ac9f7c13dddd",
+"assets/packages/fluttertoast/assets/toastify.css": "a85675050054f179444bc5ad70ffc635",
+"assets/packages/fluttertoast/assets/toastify.js": "e7006a0a033d834ef9414d48db3be6fc",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "6e4cde00bcc499b621f0fabb40316ee4",
-"/": "6e4cde00bcc499b621f0fabb40316ee4",
-"main.dart.js": "8a3107ea42ad37933d3b300bc3e00e37",
+"index.html": "dd5d0760e92deb350c3a87dd67b33c62",
+"/": "dd5d0760e92deb350c3a87dd67b33c62",
+"main.dart.js": "9982737cd932c0469d8999444015039d",
 "manifest.json": "1b6a9ce60534f9f58484ab4830816783",
-"version.json": "29bf4eada3054935329d8e55d767f5cc"
+"version.json": "29bf4eada3054935329d8e55d767f5cc",
+"_config.yml": "d178df8a46be3ea7f599296e24430ce0"
 };
 
 // The application shell files that are downloaded before a service worker can
@@ -73,7 +90,7 @@ self.addEventListener("install", (event) => {
   return event.waitUntil(
     caches.open(TEMP).then((cache) => {
       return cache.addAll(
-        CORE.map((value) => new Request(value, {'cache': 'reload'})));
+        CORE.map((value) => new Request(value + '?revision=' + RESOURCES[value], {'cache': 'reload'})));
     })
   );
 });
